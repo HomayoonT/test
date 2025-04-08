f02b01ee47d0085852ba2551c544134c13d5c6d6
DESKTOP-7SEF71P\root <h.tarbiyati@gmail.com>
2025-04-09 01:22:23 +0330

    Updated script to test profile creation

--- test_selenium.py
+++ test_selenium.py
@@ -1,7 +1,16 @@
+import os
 from seleniumbase import Driver
 
-driver = Driver(uc=True)
+CHROME_PROFILE_PATH = os.path.abspath("chrome_profile")
+
+
+is_new_profile = not os.path.exists(CHROME_PROFILE_PATH)
+if is_new_profile:
+    print("🆕 New Chrome profile created.")
+    os.makedirs(CHROME_PROFILE_PATH)
+
+driver = Driver(uc=True, headless=False, incognito=False, user_data_dir=CHROME_PROFILE_PATH)
 url = "https://www.browserscan.net/bot-detection"
 driver.uc_open(url)
-driver.sleep(10000)
+driver.sleep(5)
 driver.quit()
